a4e79ecf7dc43e3d017ddc6866150b9d7210981c
aquaruiz <gpetiaa@gmail.com>
2019-06-26 18:40:07 +0300

    just started gringotts

---
+++ HibernateCodeFirst-Exercise/Gringotts/src/main/java/App.java
@@ -0,0 +1,15 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	EntityManagerFactory factory = Persistence.createEntityManagerFactory("shampoo");
+    	EntityManager entityManager = factory.createEntityManager();
+    	    	
+    	Runnable runnable = new Engine(entityManager);
+    	runnable.run();	
+    }
+}
---
+++ HibernateCodeFirst-Exercise/Gringotts/src/main/java/Engine.java
@@ -0,0 +1,18 @@
+import javax.persistence.EntityManager;
+
+public class Engine implements Runnable{
+	private final EntityManager entityManager;
+	
+	public Engine(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+	
+	@Override
+	public void run() {
+		this.entityManager.getTransaction().begin();
+
+		
+		this.entityManager.getTransaction().commit();
+		this.entityManager.clear();
+	}
+}
