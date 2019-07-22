408541a0793427be978a6d7ee5e3878ee3cd6cee
aquaruiz <gpetiaa@gmail.com>
2019-07-22 22:15:04 +0300

    implemented JPA persistence

--- test exams/ColonialCouncilBank/src/main/java/app/ccb/CcbApplication.java
+++ test exams/ColonialCouncilBank/src/main/java/app/ccb/CcbApplication.java
@@ -3,10 +3,17 @@ package app.ccb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 @SpringBootApplication
 public class CcbApplication {
 
     public static void main(String[] args) {
+//        EntityManagerFactory factory = Persistence.createEntityManagerFactory("ccb_db");
+//        EntityManager em = factory.createEntityManager();
+
         SpringApplication.run(CcbApplication.class, args);
     }
 }
